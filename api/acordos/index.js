66f47eac1d8fe44581138ffe1bf97bb00bf6c1a1
Andre Vigarani <andre_vigarani@hotmail.com>
2026-08-05 17:01:57 -0300

    fix: acordos/index.js extrai params da URL para buscar/atualizar/:id

--- api/acordos/index.js
+++ api/acordos/index.js
@@ -1,3 +1,11 @@
 'use strict';
-// Wrapper para que GET/POST /api/acordos roteie para o handler catch-all
-module.exports = require('./[[...params]]');
+// Roteia /api/acordos e /api/acordos/* para o handler catch-all,
+// extraindo os segmentos do path como req.query.params
+const handler = require('./[[...params]]');
+module.exports = (req, res) => {
+  const path = (req.url || '').split('?')[0];
+  const after = path.replace(/^\/api\/acordos\/?/, '');
+  req.query = req.query || {};
+  req.query.params = after ? after.split('/').filter(Boolean) : [];
+  return handler(req, res);
+};
